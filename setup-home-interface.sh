88361d0bc935072857dd77fe1a69836d5c1a5a84
Zalos <satya123m4@gmail.com>
2025-06-09 21:12:16 +0530

    fix: setup bash

--- setup-home-interface.sh
+++ setup-home-interface.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
 session="Home-Interface"
+project_dir="~/Documents/home-interface" 
 
 tmux new-session -d -s $session
 
-tmux rename-window -t 0 "Server"
-tmux send-keys -t "Server" "sudo npm run start" C-m "1234" C-m
-
-
+tmux rename-window -t $session:0 "Server"
+tmux send-keys -t "$session:0" "cd $project_dir" C-m "sudo npm run start" C-m "1234" C-m
+ 
 tmux new-window -t $session:1 -n "Ngrok"
-tmux send-keys -t "Ngrok" "ngrok http --host-header=localhost 6969" C-m 
+tmux send-keys -t "$session:1" "ngrok http --host-header=localhost 6969" C-m
 
 tmux new-window -t $session:2 -n "Domain Sync"
-tmux send-keys -t "Domain Sync" "node ngrok-sync" C-m 
-
+tmux send-keys -t "$session:2" "cd $project_dir" C-m "node ngrok-sync" C-m
 
